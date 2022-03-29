3d5b4770f34b9fbf66b3d50268712bd9a91e1c39
cxOrz <cxOrz@outlook.com>
2022-03-29 21:55:21 +0800

    完成所有页面；完成DashBoard；完成所有签到方式；完成动效；

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,13 @@
-const baseUrl = 'http://localhost:5000'
+const baseUrl = 'http://10.85.15.30:5000'
 
 export const login_api = baseUrl + '/login'
+
+export const activity_api = baseUrl + '/activity'
+
+export const general_api = baseUrl + '/general'
+
+export const photo_api = baseUrl + '/photo'
+
+export const qrcode_api = baseUrl + '/qrcode'
+
+export const location_api = baseUrl + '/location'
