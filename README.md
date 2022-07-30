33484c2b0835e9bdcbdd234b83d397c70a6ec0db
cxOrz <cxOrz@outlook.com>
2022-07-30 21:57:37 +0800

    抽离签到函数; 提取过渡动画函数;

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 
 `src/config/api.js` ：该文件中配置 `baseUrl` 变量为接口地址，默认 `http://localhost:5000` 。若接口部署在服务器，不要忘记将接口地址改为服务器IP或绑定的域名。
 
-`src/pages/DashBoard/DashBoard.tsx` ：若使用腾讯云的通用文字识别服务，二维码识别成功率极高，支持PNG、JPG、JPEG、PDF等格式，图片大小不能超过7M。查看此源码文件中的 parseEnc 函数，可注释掉默认函数，使用推荐函数，同时需要在CLI项目中的 `env.json` 中配置 `secretId` 和 `secretKey`。
+`src/pages/DashBoard/Helper.ts` ：若使用腾讯云的通用文字识别服务，二维码识别成功率极高，支持PNG、JPG、JPEG、PDF等格式，图片大小不能超过7M。查看此源码文件中的 parseEnc 函数，可注释掉默认函数，使用推荐函数，同时需要在CLI项目中的 `env.json` 中配置 `secretId` 和 `secretKey`。
 
 ## 最佳实践
 
--- src/pages/DashBoard/DashBoard.tsx
+++ src/pages/DashBoard/DashBoard.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Box } from '@mui/system'
-import { Decoder } from '@nuintun/qrcode'
 import { useParams } from 'react-router-dom'
-import { activity_api, general_api, login_api, location_api, qrcode_api, photo_api, upload_api, uvtoken_api, ocr_api } from '../../config/api'
+import { activity_api, login_api } from '../../config/api'
 import { Alert, ButtonBase, CircularProgress, Snackbar } from '@mui/material'
 import './DashBoard.css'
 import { UserParamsType } from '../../types/global'
 import { AlertColor } from '@mui/material'
+import { generalSign, getuvToken, locationSign, parseEnc, photoSign, qrcodeSign, showResultWithTransition, uploadFile } from './Helper'
 
 interface SignInfo {
   activity: Activity
@@ -15,7 +15,7 @@ interface SignInfo {
 }
 interface Activity {
   name: string;
-  aid?: string | number;
+  aid?: number;
   courseId?: string | number;
   classId?: string | number;
   otherId?: string | number;
@@ -73,78 +73,27 @@ function DashBoard() {
     }
   }
 
-  const generalSign = async () => {
-    let result = await axios.post(general_api, {
-      uf: userParams.uf,
-      _d: userParams._d,
-      vc3: userParams.vc3,
-      uid: userParams._uid,
-      fid: userParams.fid,
-      aid: sign.activity.aid,
-      name: userParams.name
-    })
-    return result.data
-  }
-  const photoSign = async (objectId: string) => {
-    let result = await axios.post(photo_api, {
-      uf: userParams.uf,
-      _d: userParams._d,
-      vc3: userParams.vc3,
-      uid: userParams._uid,
-      fid: userParams.fid,
-      aid: sign.activity.aid,
-      name: userParams.name,
-      objectId: objectId
-    })
-    return result.data
-  }
-  const qrcodeSign = async (enc: string) => {
-    let result = await axios.post(qrcode_api, {
-      uf: userParams.uf,
-      _d: userParams._d,
-      vc3: userParams.vc3,
-      uid: userParams._uid,
-      fid: userParams.fid,
-      aid: sign.activity.aid,
-      name: userParams.name,
-      enc: enc
-    })
-    return result.data
-  }
-  const locationSign = async (lat: string, lon: string, address: string) => {
-    let result = await axios.post(location_api, {
-      uf: userParams.uf,
-      _d: userParams._d,
-      vc3: userParams.vc3,
-      uid: userParams._uid,
-      fid: userParams.fid,
-      aid: sign.activity.aid,
-      name: userParams.name,
-      lat: lat,
-      lon: lon,
-      address: address
-    })
-    return result.data
-  }
-
   const handleRadio = (type: 'general' | 'photo') => {
-    let label_general = document.getElementById('label-general')
-    let label_photo = document.getElementById('label-photo')
-    switch (type) {
-      case 'general': {
-        label_general!.className = 'checked'
-        label_photo!.className = 'unchecked'
-        setRadio(0)
-        break
+    return function () {
+      console.log(type)
+      let label_general = document.getElementById('label-general')
+      let label_photo = document.getElementById('label-photo')
+      switch (type) {
+        case 'general': {
+          label_general!.className = 'checked'
+          label_photo!.className = 'unchecked'
+          setRadio(0)
+          break
+        }
+        case 'photo': {
+          label_general!.className = 'unchecked'
+          label_photo!.className = 'checked'
+          setAlert({ msg: '确保已将照片上传指定位置，点击签到', severity: 'info', show: true })
+          setRadio(1)
+          break
+        }
+        default: break
       }
-      case 'photo': {
-        label_general!.className = 'unchecked'
-        label_photo!.className = 'checked'
-        setAlert({ msg: '确保已将照片上传指定位置，点击签到', severity: 'info', show: true })
-        setRadio(1)
-        break
-      }
-      default: break
     }
   }
   const updateValue = (name: string, value: string | File) => {
@@ -174,71 +123,29 @@ function DashBoard() {
   const onSign_0 = async () => {
     let res: string
     if ((document.getElementById('general') as HTMLInputElement)!.checked) {
-      res = await generalSign()
+      res = await generalSign(userParams, sign.activity.aid);
     } else {
       setBtnProgress(true)
       // 获取uvtoken
-      let token = await getuvToken()
+      let token = await getuvToken(userParams);
       // 上传文件，获取上传结果
-      let result_upload = await uploadFile(values['photo'] as File, token)
+      let result_upload = await uploadFile(userParams, values['photo'] as File, token);
       console.log(result_upload)
       // 传入objectId进行签到
-      res = await photoSign(result_upload.objectId)
+      res = await photoSign(userParams, sign.activity.aid, result_upload.objectId);
       setBtnProgress(false)
     }
-    (document.getElementById('sign-btn') as HTMLButtonElement)!.disabled = true
-    let neum_form = document.getElementsByClassName('neum-form')[0]
-    let content = document.getElementById('neum-form-content')
-    content!.style.opacity = '0'
-    setTimeout(() => {
-      content!.style.display = 'none'
-      neum_form.classList.add('form-height')
-      setStatus(res)
-    }, 600)
+    showResultWithTransition(setStatus, res);
   }
   const onSign_2 = async () => {
-    let res = await qrcodeSign(values['enc'] as string);
-    (document.getElementById('sign-btn') as HTMLButtonElement)!.disabled = true
-    let neum_form = document.getElementsByClassName('neum-form')[0]
-    let content = document.getElementById('neum-form-content')
-    content!.style.opacity = '0'
-    setTimeout(() => {
-      content!.style.display = 'none'
-      neum_form.classList.add('form-height')
-      setStatus(res)
-    }, 600)
+    let res = await qrcodeSign(userParams, sign.activity.aid, values['enc'] as string);
+    showResultWithTransition(setStatus, res);
   }
-  // [默认] 使用浏览器解析ENC，成功率较低
-  const parseEnc = (file: File): Promise<string> => {
-    return new Promise((resolve) => {
-      const url = window.URL || window.webkitURL
-      const img = new Image()
-      const qrcode = new Decoder()
-      img.src = url.createObjectURL(file)
-      qrcode.scan(img.src).then(result => {
-        resolve(result.data.split('=').pop() as string)
-      }).catch((reason) => {
-        console.log(reason)
-        resolve('识别失败')
-      })
-    })
-  }
-  // [推荐] 使用腾讯云OCR解析ENC，请在cli项目中配置secretId和secretKey
-  // const parseEnc = async (inputFile: File) => {
-  //   let data = new FormData()
-  //   data.append("file", inputFile)
-  //   let res = await axios.post(ocr_api, data, {
-  //     headers: {
-  //       'Content-type': 'multipart/form-data'
-  //     }
-  //   })
-  //   return res.data
-  // }
   const setEncByQRCodeImage = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const image = event.target.files![0]
     setScanProgress(true)
     // 对图片文件进行解析获得enc
-    const enc = await parseEnc(image)
+    const enc = await parseEnc(image);
     values['enc'] = enc
     const encInput = document.getElementById('input-enc')
     encInput!.setAttribute('value', enc)
@@ -246,55 +153,13 @@ function DashBoard() {
   }
   const onSign_4 = async () => {
     let latlon = values['latlon'] as string, address = values['address'] as string
-    let res = await locationSign(latlon.substring(latlon.indexOf(',') + 1, latlon.length),
+    let res = await locationSign(userParams, sign.activity.aid, latlon.substring(latlon.indexOf(',') + 1, latlon.length),
       latlon.substring(0, latlon.indexOf(',')), address);
-    (document.getElementById('sign-btn') as HTMLButtonElement)!.disabled = true
-    let neum_form = document.getElementsByClassName('neum-form')[0]
-    let content = document.getElementById('neum-form-content')
-    content!.style.opacity = '0'
-    setTimeout(() => {
-      content!.style.display = 'none'
-      neum_form.classList.add('form-height')
-      setStatus(res)
-    }, 600)
+    showResultWithTransition(setStatus, res);
   }
   const onSign_35 = async () => {
-    let res = await generalSign();
-    (document.getElementById('sign-btn') as HTMLButtonElement).disabled = true
-    let neum_form = document.getElementsByClassName('neum-form')[0]
-    let content = document.getElementById('neum-form-content')
-    content!.style.opacity = '0'
-    setTimeout(() => {
-      content!.style.display = 'none'
-      neum_form.classList.add('form-height')
-      setStatus(res)
-    }, 600)
-  }
-  const getuvToken = async () => {
-    let token = await axios.post(uvtoken_api, {
-      uf: userParams.uf,
-      _d: userParams._d,
-      vc3: userParams.vc3,
-      uid: userParams._uid
-    })
-    return token.data._token
-  }
-  const uploadFile = async (inputFile: File, token: string) => {
-    // 填入FormData
-    let data = new FormData()
-    data.append('uf', userParams.uf)
-    data.append('_d', userParams._d)
-    data.append('_uid', userParams._uid)
-    data.append('vc3', userParams.vc3)
-    data.append('file', inputFile)
-
-    // 使用Token传文件，返回objectId
-    let res = await axios.post(upload_api + `?_token=${token}`, data, {
-      headers: {
-        'Content-type': 'multipart/form-data'
-      }
-    })
-    return res.data
+    let res = await generalSign(userParams, sign.activity.aid);
+    showResultWithTransition(setStatus, res);
   }
 
   useEffect(() => {
@@ -370,11 +235,11 @@ function DashBoard() {
           <h3>{sign.status}</h3>
           <div id='neum-form-content' className='form-content'>
             <p className='form-title'>勾选签到方式</p><br />
-            <label id='label-general' onClick={() => { handleRadio('general') }} className='checked' htmlFor='general' style={{ fontSize: '1.6rem' }}>
+            <label id='label-general' onClick={handleRadio('general')} className='checked' htmlFor='general' style={{ fontSize: '1.6rem' }}>
               <input hidden defaultChecked type='radio' name='sign' id='general' value='general' />
               &nbsp;普通
             </label>&emsp;
-            <label id='label-photo' onClick={() => { handleRadio('photo') }} className='unchecked' htmlFor='photo' style={{ fontSize: '1.6rem' }}>
+            <label id='label-photo' onClick={handleRadio('photo')} className='unchecked' htmlFor='photo' style={{ fontSize: '1.6rem' }}>
               <input hidden type='radio' name='sign' id='photo' value='photo' />
               &nbsp;拍照
             </label>
